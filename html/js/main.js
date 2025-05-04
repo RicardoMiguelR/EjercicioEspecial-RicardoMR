0dfe5dd6e5509a8ba448aae5a7f30422254a3b68
Ricardo MR <ricardomr.23@hotmail.com>
2025-05-04 02:52:54 -0600

    Se sube parte 2 de ejercicio, mostrar cards con datos

--- html/js/main.js
+++ html/js/main.js
@@ -1,28 +1,36 @@
 const loadProducts = document.getElementById("loadProducts");
 const insertCards = document.getElementById("insertCards");
-const apiURL = "https://api.escuelajs.co/api/v1/products";
 const alertaError = document.getElementById("alertaError");
+const apiURL = "https://api.escuelajs.co/api/v1/products";
 
-const llamarApi = () => {
-  loadProducts.addEventListener("click", (e) => {
+function getData() {
+  return new Promise((resolve, reject) => {
+    fetch(apiURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al traer los datos");
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+
+const llamarApi = async () => {
+  loadProducts.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    function getData() {
-      return new Promise((resolve, reject) => {
-        if (apiURL == null) {
-          reject(new Error("Error al obtener los datos"));
-        }
-        resolve(apiURL);
-      });
-    }
-
-    const fetch = async () => {
-      try {
-        const apiStore = await getData();
-        const mostrarCards = apiStore.map((producto) => {
+    try {
+      const apiStore = await getData();
+      const mostrarCards = apiStore
+        .slice(0, 9)
+        .map((producto) => {
           return `
+                  <div class="col">
                     <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <img src="${producto.images[1]}" referrerpolicy="no-referrer" alt="imagen-prod">
                         <div class="card-body">
                             <h1>${producto.title}</h1>
                             <p class="card-text">${producto.description}</p>
@@ -35,26 +43,28 @@ const llamarApi = () => {
                             </div>
                         </div>
                     </div>
-                `;
-        });
-        insertCards.innerHTML = mostrarCards;
-      } catch (error) {
-        alertaError.insertAdjacentHTML(
-          "beforeend",
+                  </div>
+              `;
+        })
+        .join("");
+      insertCards.innerHTML = mostrarCards;
+    } catch (error) {
+      alertaError.insertAdjacentHTML(
+        "beforeend",
+        `
+              <div class="alert alert-danger d-flex" role="alert" style="justify-content: center;">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="bi flex-shrink-0 me-2" width="50px" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>
+                  <div>
+                      <strong>${error.message}</strong>
+                  </div>
+              </div>
           `
-                <div class="alert alert-danger d-flex" role="alert">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="bi flex-shrink-0 me-2" width="50px" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                    </svg>
-                    <div>
-                        <strong>${error.message}</strong>
-                    </div>
-                </div>
-            `
-        );
-      }
-    };
+      );
+    }
   });
 };
+llamarApi();
 
 // Ejercicio por terminar ... ******
